88aa957aeec80d274777350207697a7ac66c8c56
Eros628 <eroslucagbo314@gmail.com>
2026-05-09 16:15:47 +0800

    feat: Added a auto suggestion for places when input

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,60 @@
 import { use, useState } from 'react'
 import axios from 'axios'
 import './App.css'
-import { getWeather } from './service/api.js';
+import { getPlaces, getWeather } from './service/api.js';
+import { SearchIcon } from 'lucide-react';
 
 function App() {
   const [location, setLocation] = useState('');
+  const [places, setPlaces] = useState([]);
   const getApi =  async ()=>{
       const data = await getWeather(location);
       console.log(data);
   }
 
+  const setInput = async(place)=>{
+    const response =  await getPlaces(place);
+    const listPlaces = response.features.map((place)=>{
+      return place.properties.name +", "+place.properties.country;
+    })
+
+    setPlaces(listPlaces);
+  }
+
+
   return (
     <>
-      <form onSubmit={(e)=>{
-        e.preventDefault();
-        getApi();
-      }}>
-        <input onChange={
-          (e)=>{
-            setLocation(e.target.value);
-          }
-        } placeholder='enter location'></input>
-        <button type='submit'> SEARCH</button>
-      </form>
-
-      <div>
-
+      <div className='main-container'>
+        <div className='search-container'>
+          <h1>WeatherWise</h1>
+          <form onSubmit={(e)=>{
+              getApi(e.target.value);
+          }}>
+            <div className='input-suggest-wrapper'>
+              <div className='input-wrapper'>
+                <input onChange={
+                  (e)=>{
+                    if(e.target.value == ""){
+                      setPlaces([]);
+                    }
+                    else{
+                      setInput(e.target.value);
+                    }
+                    
+                    
+                  }
+                } className='input-bar' placeholder='Enter your City'></input>
+                <button className='search-btn' type='submit'><SearchIcon></SearchIcon></button>
+              </div>
+               {places.length > 0 && 
+              <div className='places-suggestions-wrapper'>
+                {places.map((place, index)=>{
+                  return <div className='list-suggest' key={index}>{place}</div>
+                })}
+              </div>}
+            </div>
+          </form>
+        </div>
       </div>
     </>
   )
--- src/service/api.js
+++ src/service/api.js
@@ -13,3 +13,19 @@ export async function getWeather(location){
       console.log(error);
     }
 }
+
+export async function  getPlaces (location) {
+    try {
+        const response = await axios.get("https://photon.komoot.io/api", {
+            params:{
+                q: location,
+                limit: 5
+            }
+        });
+    
+        return response.data;
+
+    } catch (error) {
+        console.log(error);
+    }
+}
